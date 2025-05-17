0d6223bbc8733687da1ae7b2b152b0ddf52689ed
Jacob Booth <88772843+jsb-dev@users.noreply.github.com>
2025-05-17 14:11:14 +1000

    Documentation

--- mongo-secret.sh
+++ mongo-secret.sh
@@ -1,3 +1,6 @@
+# Similarly to mongo-setup.sh, I moved this to a job which could be applied to the deployment,
+# automating the process and bringing everything over to yaml
+
 # Set MongoDB secret in bash
 MONGO_USERNAME="mongouser"
 MONGO_PASSWORD="mongopassword"
@@ -7,6 +10,8 @@ kubectl create secret generic mongodb-secret \
   --from-literal=MONGO_INITDB_ROOT_USERNAME=mongouser \
   --from-literal=MONGO_INITDB_ROOT_PASSWORD=mongopassword \
   --from-literal=MONGO_INITDB_DATABASE=calculator
+# I noticed that I never configured the connection string as a secret since I had it hardcoded 
+# through development, I address this in the next submission for better security
 
 # Display the secret for reference
 echo "MongoDB secret created and stored in Kubernetes secret 'mongodb-secret'"
