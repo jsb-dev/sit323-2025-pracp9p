cf84ba3d7614c95d0060326de197255d84e6ed62
Jacob Booth <88772843+jsb-dev@users.noreply.github.com>
2025-05-17 14:07:43 +1000

    Documentation

--- mongo-setup.sh
+++ mongo-setup.sh
@@ -1,9 +1,12 @@
+# Following this submission, I ended up moving this script to a one-time job that I can apply to the deployment,
+# this streamlined the subsequent deployment to GCP for 10.1P
+
 # Read in values and decode them
 MONGO_USERNAME=$(kubectl get secret mongodb-secret -o jsonpath='{.data.MONGO_INITDB_ROOT_USERNAME}' | base64 --decode)
 MONGO_PASSWORD=$(kubectl get secret mongodb-secret -o jsonpath='{.data.MONGO_INITDB_ROOT_PASSWORD}' | base64 --decode)
 MONGO_DATABASE=$(kubectl get secret mongodb-secret -o jsonpath='{.data.MONGO_INITDB_DATABASE}' | base64 --decode)
 
-# Set default values if any variable is empty
+# Set default values if any variable is empty, helped when debugging the secret config to determine which passed
 MONGO_USERNAME=${MONGO_USERNAME:-"mongouser"}
 MONGO_PASSWORD=${MONGO_PASSWORD:-"mongopassword"}
 MONGO_DATABASE=${MONGO_DATABASE:-"defaultdb"}
@@ -14,7 +17,7 @@ echo "Username: $MONGO_USERNAME"
 echo "Password: $MONGO_PASSWORD"
 echo "Database: $MONGO_DATABASE"
 
-# Wait for MongoDB to start, gives pods a chance to spin up if they haven't already
+# Wait for MongoDB to start, gives pod a chance to spin up if they haven't already
 echo "Waiting for MongoDB to start..."
 sleep 10
 
